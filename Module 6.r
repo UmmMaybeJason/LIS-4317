25572c09940ca4771c227bcbb1a0537c1f32d456
NotJason <46411801+UmmMaybeJason@users.noreply.github.com>
2025-02-23 03:45:26 -0500

    Add files via upload

---
+++ Module 6.r
@@ -0,0 +1,15 @@
+library(ggplot2)
+
+names <- c("Jeric", "Savannah", "Shenica", "Anika", "Tristan", "Anna", "Bria", "Karla", "Angelica", "Quang","Sarieli", "Mikayla", "Paige", "Ari", "Ben")
+snipes <- c(64, 61, 38, 35, 27, 25, 24, 21, 20, 20, 19, 15, 15, 13, 13)
+data <- data.frame(names, snipes)
+print(data)
+
+plot <- ggplot(data, aes(x = names, y = snipes, fill = snipes)) + 
+  geom_col() + 
+  coord_flip() +
+  labs(title = "Dance Team Snipes", x = "Name", y = "Snipes") +
+  scale_fill_gradient(low = "blue", high = "black")
+
+
+print(plot)
