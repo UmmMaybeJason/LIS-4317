efc4606d2d4d5728775719c04880a3c199db83cf
NotJason <46411801+UmmMaybeJason@users.noreply.github.com>
2025-04-07 01:57:27 -0400

    Add files via upload

---
+++ Module 11.R
@@ -0,0 +1,32 @@
+# Module 11
+
+library("CarletonStats")
+library("devtools")
+library("epanetReader")
+library("fmsb")
+library("ggplot2")
+library("ggthemes")
+library("latticeExtra")
+library("MASS")
+library("PerformanceAnalytics")
+library("psych")
+library("plyr")
+library("prettyR")
+library("plotrix")
+library("proto")
+library("RCurl")
+library("reshape")
+library("reshape2")
+
+x <- 1967:1977
+y <- c(0.5,1.8,4.6,5.3,5.3,5.7,5.4,5,5.5,6,5)
+pdf(width=10, height=6)
+plot(y ~ x, axes=F, xlab="", ylab="", pch=16, type="b")
+axis(1, at=x, label=x, tick=F, family="serif")
+axis(2, at=seq(1,6,1), label=sprintf("$%s", seq(300,400,20)), tick=F, las=2, family="serif")
+abline(h=6,lty=2)
+abline(h=5,lty=2)
+text(max(x), min(y)*2.5,"Per capita\nbudget expanditures\nin constant dollars", adj=1, 
+     family="serif")
+text(max(x), max(y)/1.08, labels="5%", family="serif")
+dev.off()
